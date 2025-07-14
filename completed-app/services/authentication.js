e08972c3811ad2f9caa30d9aff11922c18a48480
RichardJECooke <RichardJECooke@pm.me>
2025-07-14 16:11:02 +0200

    try again on indentation

--- completed-app/services/authentication.js
+++ completed-app/services/authentication.js
@@ -3,9 +3,10 @@ const session = require("express-session");
 const passport = require("passport");
 const jwt_decode = require("jwt-decode");
 const OauthStrategy = require("passport-oauth2").Strategy;
+
 //tag::a[]
-  const SamlStrategy = require("@node-saml/passport-saml").Strategy;
-  const fs = require('fs');
+const SamlStrategy = require("@node-saml/passport-saml").Strategy;
+const fs = require('fs');
 //end::a[]
 
 function setupPassport(app) {
